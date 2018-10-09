1336e8464b1d94d5b5bdbdb097ef09a744d7a08d
Benktesh Sharma <benktesh@yahoo.com>
2018-10-08 19:03:41 -0500

    code reformat

--- smartstock/app/src/main/java/benktesh/smartstock/Data/Contract.java
+++ smartstock/app/src/main/java/benktesh/smartstock/Data/Contract.java
@@ -5,11 +5,10 @@ import android.provider.BaseColumns;
 public class Contract {
 
     public static final class PortfolioEntry implements BaseColumns {
-
-        public static  final String TABLE_NAME = "portfolio";
+        public static final String TABLE_NAME = "portfolio";
 
         public static final String COLUMN_SYMBOL = "symbol";
-        public  static final String COLUMN_CHANGE = "change";
+        public static final String COLUMN_CHANGE = "change";
         public static final String COLUMN_INPORTFOLIO = "inPortfolio";
         public static final String COLUMN_MARKET = "market";
         public static final String COLUMN_DAY_HIGH = "dayHigh";
@@ -20,11 +19,10 @@ public class Contract {
     }
 
     public static final class MarketEntry implements BaseColumns {
-
-        public static  final String TABLE_NAME = "market";
+        public static final String TABLE_NAME = "market";
 
         public static final String COLUMN_CHANGESYMBOL = "symbol";
-        public  static final String COLUMN_ = "change";
+        public static final String COLUMN_ = "change";
         public static final String COLUMN_VALUE = "value";
         public static final String COLUMN_DAY_HIGH = "dayHigh";
         public static final String COLUMN_DAY_LOW = "dayLow";
