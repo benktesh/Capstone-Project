af0c79e5e5267aa63f04b26deccaa75b6403b72e
Benktesh Sharma <benktesh@yahoo.com>
2018-10-12 06:43:04 -0500

    adds a constant

--- smartstock/app/src/main/java/benktesh/smartstock/Utils/SmartStockConstant.java
+++ smartstock/app/src/main/java/benktesh/smartstock/Utils/SmartStockConstant.java
@@ -4,4 +4,5 @@ public class SmartStockConstant {
     public static String PortfolioQueryString = "SmartStockPortfolio";
     public static String QueryStock = "SmartStockStock";
     public static String QueryMarket = "SmartStockMarket";
+    public static int MaximumSearchResult = 10;
 }
