3d43aa7f6109c4d66d5eacabeb72fbce298ed9ec
Benktesh Sharma <benktesh@yahoo.com>
2018-10-28 11:27:03 -0500

    Create a class for symbol

---
+++ smartstock/app/src/main/java/benktesh/smartstock/Model/Symbol.java
@@ -0,0 +1,53 @@
+package benktesh.smartstock.Model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Symbol implements Parcelable {
+    public String symbol;
+    public String name;
+    public String date;
+    public String isenabled;
+    public String type;
+    public String iexid;
+
+    public Symbol() {
+
+    }
+
+    protected Symbol(Parcel in) {
+        symbol = in.readString();
+        name = in.readString();
+        date = in.readString();
+        isenabled = in.readString();
+        type = in.readString();
+        iexid = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(symbol);
+        dest.writeString(name);
+        dest.writeString(date);
+        dest.writeString(isenabled);
+        dest.writeString(type);
+        dest.writeString(iexid);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Symbol> CREATOR = new Creator<Symbol>() {
+        @Override
+        public Symbol createFromParcel(Parcel in) {
+            return new Symbol(in);
+        }
+
+        @Override
+        public Symbol[] newArray(int size) {
+            return new Symbol[size];
+        }
+    };
+}
